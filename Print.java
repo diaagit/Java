1f623083ba28d765e3c38032ef9ccd785d2026f1
Diya Jain <diyajain@Diyas-MacBook-Air.local>
2025-07-09 16:26:36 +0530

    Output

---
+++ Print.java
@@ -0,0 +1,24 @@
+public class Print {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints messages to the console. 
+        
+        //println is used to print a line with a newline character at the end,
+        
+        System.out.println("Hello, World!");
+
+        // print does not add a new line at the end of the output.
+
+        System.out.print("This is a simple Java program. lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+
+
+        //\n is used to add a new line in the output.
+         System.out.println("This is a simple Java program. lorem ipsum dolor sit amet, consectetur adipiscing elit. \n");
+
+
+         // print does not add a new line at the end of the output.
+
+        System.out.print("This is a simple Java program. lorem ipsum dolor sit amet, consectetur adipiscing elit. \n");
+        
+         System.out.println("This is a simple Java program. lorem ipsum dolor sit amet, consectetur adipiscing elit. ");
+    }
+}
