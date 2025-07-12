119da09a03bfb338662f37219a700180c454970e
Diya Jain <diyajain@Diyas-MacBook-Air.local>
2025-07-12 17:44:56 +0530

    Functions

---
+++ Average.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Average {
+    public static int printAverage(int num1 , int num2, int num3){
+        int average = (num1 + num2 +num3)/3;
+        System.out.println("The average of the three numbers is: " + average);
+        return average;
+    }
+    public static void main( String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+        printAverage(num1, num2, num3);
+
+    }
+}
+ 
