543516b34346e2fbc8f7961929e01153cefc7834
diya jain <149897850+diaagit@users.noreply.github.com>
2026-01-28 18:35:42 +0530

    Implement binary search algorithm in Java

---
+++ Binary_search.java
@@ -0,0 +1,23 @@
+class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int key = 8;
+
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index " + mid);
+                return;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
